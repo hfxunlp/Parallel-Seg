028b22d724eb56d96c02cd4c1489a41f349776d3
ano <anoidgit@users.noreply.github.com>
2017-03-13 17:17:42 +0800

    Update seg.py

--- seg.py
+++ seg.py
@@ -28,6 +28,6 @@ def segfile(srcfile,rsfile):
 
 if __name__=="__main__":
 	nlpir.Init(nlpir.PACKAGE_DIR,nlpir.UTF8_CODE,None)
-	nlpir.SetPOSmap(nlpir.PKU_POS_MAP_SECOND)#ICT_POS_MAP_SECOND/FIRST
+	#nlpir.SetPOSmap(nlpir.PKU_POS_MAP_SECOND)#ICT_POS_MAP_SECOND/FIRST
 	segfile(sys.argv[1].decode("utf-8"),sys.argv[2].decode("utf-8"))
 	nlpir.Exit()
