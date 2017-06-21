5fdaecd2a5ed7deef433d3f215dab39cd1e44699
ano <anoidgit@users.noreply.github.com>
2017-06-21 17:57:15 +0800

    Update postag.py

--- postag.py
+++ postag.py
@@ -2,27 +2,29 @@
 
 import sys
 from pyltp import Postagger
+ 
+ltpdata="/media/Storage/data/ltp_data/"
+posmfile=ltpdata+"pos.model" 
 
-def tagline(srcl, ptag):
+def handleline(srcl, ptag):
 	wds=srcl.split()
 	tags=ptag.postag(wds)
 	rs=["/".join(mu) for mu in zip(wds,tags)]
 	return " ".join(rs)
 
 def handle(srcf, rsf):
-	global mfile
+	global posmfile
 	with open(srcf) as frd:
 		ptagger=Postagger()
-		ptagger.load(mfile)
+		ptagger.load(posmfile)
 		with open(rsf, "w") as fwrt:
 			for line in frd:
 				tmp=line.strip()
 				if tmp:
 					tmp=tmp.decode("utf-8","ignore")
-					tmp=tagline(tmp, ptagger)
+					tmp=handleline(tmp, ptagger)
 					fwrt.write(tmp.encode("utf-8","ignore"))
 				fwrt.write("\n".encode("utf-8"))
 
 if __name__=="__main__":
-	mfile="/media/Storage/data/ltp_data/pos.model"
 	handle(sys.argv[1].decode("utf-8"), sys.argv[2].decode("utf-8"))
