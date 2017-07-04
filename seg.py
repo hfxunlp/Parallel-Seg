0c241d55988ae5cacfa8cca280f2a06695e43aa1
ano <anoidgit@users.noreply.github.com>
2017-07-04 21:09:41 +0800

    Update seg.py

--- seg.py
+++ seg.py
@@ -10,7 +10,7 @@ def segline(strin):
 		rs=""
 	return rs.decode("utf-8")
 
-def segfile(srcfile,rsfile):
+def handle(srcfile,rsfile):
 	err=0
 	with open(rsfile,"wb") as fwrt:
 		with open(srcfile,"rb") as frd:
@@ -19,15 +19,15 @@ def segfile(srcfile,rsfile):
 				if tmp:
 					tmp=segline(tmp.decode("utf-8","ignore"))
 					if tmp:
-						tmp+="\n"
 						fwrt.write(tmp.encode("utf-8","ignore"))
 					else:
 						err+=1
+				fwrt.write("\n")
 	if err>0:
-		print("Seg:"+srcfile+",Error:"+str(err))
+		print("".join(("Seg:",srcfile,",Error:",str(err),)))
 
 if __name__=="__main__":
 	nlpir.Init(nlpir.PACKAGE_DIR,nlpir.UTF8_CODE,None)
 	#nlpir.SetPOSmap(nlpir.PKU_POS_MAP_SECOND)#ICT_POS_MAP_SECOND/FIRST
-	segfile(sys.argv[1].decode("utf-8"),sys.argv[2].decode("utf-8"))
+	handle(sys.argv[1].decode("utf-8"),sys.argv[2].decode("utf-8"))
 	nlpir.Exit()
