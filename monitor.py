75dc5271cf878d1dfc9b3926b81d939bdf811c2a
ano <anoidgit@users.noreply.github.com>
2017-07-04 19:50:48 +0800

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,6 +1,7 @@
 #encoding: utf-8
 
 import sys
+import os
 import subprocess
 import threading
 from time import sleep
@@ -15,8 +16,16 @@ def mulprint(strp):
 	with printlock:
 		print(strp)
 
-def createPool():
-	return ["src/"+str(i)+".txt" for i in xrange(1946,2006)],["rs/"+str(i)+".txt" for i in xrange(1946,2006)]
+def createPool(srcp, rsp):
+	#return ["src/"+str(i)+".txt" for i in xrange(1946,2006)],["rs/"+str(i)+".txt" for i in xrange(1946,2006)]
+	srcl=[]
+	rsl=[]
+	for root, dirs, files in os.walk(srcp):
+		for file in files:
+			srcf=os.path.join(root, file)
+			srcl.append(srcf)
+			rsl.append(srcf.replace(srcp, rsp))
+	return srcl, rsl
 
 def get():
 	global plock,srcp,rsp
@@ -46,7 +55,7 @@ def core():
 			nthread-=1
 
 if __name__=="__main__":
-	srcp,rsp=createPool()
+	srcp,rsp=createPool(sys.argv[1].decode("utf-8"), sys.argv[2].decode("utf-8"))
 	tpool=[]
 	for i in xrange(nthread):
 		t=threading.Thread(target=core)
