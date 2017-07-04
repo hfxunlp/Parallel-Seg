9bf0c2aed7976d13893301316a585146735e6f53
ano <anoidgit@users.noreply.github.com>
2017-07-04 21:10:27 +0800

    Update postag.py

--- postag.py
+++ postag.py
@@ -24,7 +24,7 @@ def handle(srcf, rsf):
 					tmp=tmp.decode("utf-8","ignore")
 					tmp=handleline(tmp, ptagger)
 					fwrt.write(tmp.encode("utf-8","ignore"))
-				fwrt.write("\n".encode("utf-8"))
+				fwrt.write("\n")
 
 if __name__=="__main__":
 	handle(sys.argv[1].decode("utf-8"), sys.argv[2].decode("utf-8"))
