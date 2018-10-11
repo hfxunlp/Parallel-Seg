ce8dda5212e39acc4e649cebd1db434bef921942
ano <anoidgit@users.noreply.github.com>
2018-10-11 19:45:13 +0800

    Update nlpirseg.py

--- nlpirseg.py
+++ nlpirseg.py
@@ -3,31 +3,56 @@
 import sys
 from pynlpir import nlpir
 
-def segline(strin):
+def segcore(strin):
 	try:
-		rs=nlpir.ParagraphProcess(strin.encode("utf-8"), 1)
+		rs=nlpir.ParagraphProcess(strin.encode("utf-8", "ignore"), 1)
 	except:
 		rs=""
 	return rs.decode("utf-8","ignore")
 
+def segline(strin):
+	def clear_tag(strin):
+		tmp = strin.split()
+		rs = []
+		for tmpu in tmp:
+			ind = tmpu.rfind("/")
+			if ind > 0:
+				rs.append(tmpu[:ind])
+			else:
+				rs.append(tmpu)
+		return " ".join(rs)
+	tmp = segcore(strin)
+	if tmp:
+		return clear_tag(tmp)
+	else:
+		return tmp
+
 def handle(srcfile,rsfile):
 	err=0
+	ens="\n".encode("utf-8")
 	with open(rsfile,"wb") as fwrt:
 		with open(srcfile,"rb") as frd:
 			for line in frd:
 				tmp=line.strip()
 				if tmp:
-					tmp=segline(tmp.decode("utf-8","ignore"))
+					try:
+						tmp = tmp.decode("utf-8")
+					except Exception as e:
+						tmp = ""
 					if tmp:
-						fwrt.write(tmp.encode("utf-8"))
+						tmp=segline(tmp)
+						if tmp:
+							fwrt.write(tmp.encode("utf-8"))
+						else:
+							err+=1
 					else:
 						err+=1
-				fwrt.write("\n")
+				fwrt.write(ens)
 	if err>0:
 		print("".join(("Seg:",srcfile,",Error:",str(err),)))
 
 if __name__=="__main__":
 	nlpir.Init(nlpir.PACKAGE_DIR,nlpir.UTF8_CODE,None)
 	#nlpir.SetPOSmap(nlpir.PKU_POS_MAP_SECOND)#ICT_POS_MAP_SECOND/FIRST
-	handle(sys.argv[1].decode("utf-8"),sys.argv[2].decode("utf-8"))
+	handle(sys.argv[1], sys.argv[2])
 	nlpir.Exit()
