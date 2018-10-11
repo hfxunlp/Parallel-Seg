26f9d6fddfdbc7fa9024985c21ce6c94a1e4026b
ano <anoidgit@users.noreply.github.com>
2018-10-11 18:58:43 +0800

    Add tool to tokenize english text

---
+++ tok.py
@@ -0,0 +1,36 @@
+#encoding: utf-8
+
+import sys
+import nltk.tokenize.moses.MosesTokenizer as MosesTokenizer
+
+def segline(strin, tok, to_lower = False):
+	def clearstr(lin, to_lower):
+		rs = []
+		for tmpu in lin:
+			if tmpu:
+				rs.append(tmpu)
+		if to_lower:
+			return " ".join(rs).lower()
+		else:
+			return " ".join(rs).lower()
+	return clearstr((tok.tokenize(core, return_str=True, escape=False).split(), to_lower)
+
+def handle(srcfile,rsfile):
+	err=0
+	with open(rsfile,"wb", encoding='utf-8', errors='ignore') as fwrt:
+		with open(srcfile,"rb", encoding='utf-8', errors='ignore) as frd:
+			tok = MosesTokenizer(lang='en')
+			for line in frd:
+				tmp=line.strip()
+				if tmp:
+					tmp=segline(tmp, tok)
+					if tmp:
+						fwrt.write(tmp)
+					else:
+						err+=1
+				fwrt.write("\n")
+	if err>0:
+		print("".join(("Seg:",srcfile,",Error:",str(err),)))
+
+if __name__=="__main__":
+	handle(sys.argv[1].decode("utf-8"),sys.argv[2].decode("utf-8"))
